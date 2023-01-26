21d26756dce298b3c564e639e12fc5d2fd4292ed
maguitaria <t2glma00@students.oamk.fi>
2023-01-26 23:24:36 +0200

    added functionality for projectiles

---
+++ script.js
@@ -0,0 +1,129 @@
+window.addEventListener('load',function(){
+     // canvas setup
+     const canvas = this.document.getElementById('canvas1');
+     const ctx = canvas.getContext('2d');
+     canvas.width = 500;
+     canvas.height = 500;
+
+     class InputHandler { // keyboard input
+constructor(game){
+    this.game = game;
+    window.addEventListener('keydown', e => {  //arrow functions always represent the object in which the arrow function os defined
+ if (
+    ( e.key === 'ArrowUp' || e.key === 'ArrowDown') && 
+    this.game.keys.indexOf(e.key)=== -1
+    ) {
+     this.game.keys.push(e.key);
+      } else if (e.key === ' ') {
+     this.game.player.shootTop();
+    }
+    });
+
+  window.addEventListener('keyup', e =>{
+if (this.game.keys.indexOf(e.key) > -1) {
+    this.game.keys.splice(this.game.keys.indexOf(e.key),1);
+}
+});
+}
+}
+class Projectile { // shooting lasers at enemies
+    constructor(game,x,y) {
+        this.game = game;
+        this.x = x;
+        this.y = y;
+        this.width = 10;
+        this.height = 3;
+        this.speed = 3;
+        this.markedForDeletion = false;
+    }
+
+
+update() { //increase line speed
+this.x =+ this.speed;
+if (this.x > this.game.width * 0.8)  this.markedForDeletion = true;
+ // if the projectile area is bigger than game area,then the object is deleted
+}
+
+draw(context) {
+    context.fillStyle = 'yellow';
+    context.fillRect(this.x, this.y, this.width, this.height)
+}
+}
+class Particle {
+
+}
+class Player {
+constructor(game){
+    this.game = game;
+    this.width = 120;
+    this.height = 190;
+    this.x = 20;
+    this.y = 100;
+    this.speedY = 0;
+    this.maxSpeed = 3;
+    this.projectiles = [];
+}
+
+update() {
+    if(this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed;
+    else if (this.game.keys.includes('ArrowDown')) this.speedY = this.maxSpeed;
+    else this.speedY = 0;
+     this.y =+ this.speedY;
+
+// handle projectiles
+this.projectiles.forEach(projectile => {
+    projectile.update();
+})
+this.projectiles = this.projectiles.filter(
+projectile => !projectile.markedForDeletion); // remove all elements from array when 'markedForDeletion' == true.Filter creates a new array, that`s why we set it to the property to ensure that it`s updated.
+}
+
+draw(context){
+    context.fillStyle = 'green';
+context.fillRect(this.x,this.y,this.width,this.height);
+this.projectiles.forEach(projectile => {
+    projectile.draw(context);
+});
+}
+shootTop() {
+    this.projectiles.push(new Projectile(this.game,this.x,this.y));
+    console.log(this.projectiles);
+}
+}
+class Enemy {
+
+}
+class Layer {
+
+}
+class Background {
+
+}
+class UI {
+
+}
+class Game {
+constructor(width,height) {
+    this.width = width;
+    this.height = height;
+    this.player = new Player(this);
+    this.input = new InputHandler(this);
+    this.keys = [];
+}
+update() {
+    this.player.update();
+}
+draw(context) {
+    this.player.draw(context);
+}
+}
+const game = new Game(canvas.width,canvas.height);
+// animation loop which will update the game every 60 sec
+function animate() {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    game.update();
+    game.draw(ctx);
+    requestAnimationFrame(animate); // callback with endless loop
+}
+animate();
+    });
